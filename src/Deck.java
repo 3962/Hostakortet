fe42e4406c99c57dfea3a98476e588d017a49d40
Leonard Bergman <bergman.leonard@elev.ga.ntig.se>
2023-05-25 21:27:25 +0200

    subclass cheater Deck for loop

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,11 @@
 public class Deck {
+    //attributes
+    Card[] deck = new Card[5];
+
+    //constructor
+public Deck(){
+    for (int i = 0; i < deck.length; i++){
+    deck[i] = new Card((i+1));
+    }
+}
 }
