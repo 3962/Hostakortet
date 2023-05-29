b1a7889e25f7b31936c504df0b0fea96c3f5eccd
Leonard Bergman <bergman.leonard@elev.ga.ntig.se>
2023-05-29 20:56:36 +0200

    subclass cheater Deck for loop card class deck class update player class cheater gameclass

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,13 @@
 public class Game {
+    private Deck deck;
+    private Player player;
+    private Cheater cheater;
+
+    public Game(){
+        deck = new Deck();
+        player1 = new Player(deck);
+        cheater = new Cheater(deck);
+    }
+
+    public void
 }
