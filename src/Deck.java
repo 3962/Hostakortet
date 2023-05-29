8a98bb8b9f9f36f262d4e8e0dcea4d504912d044
Leonard Bergman <bergman.leonard@elev.ga.ntig.se>
2023-05-29 19:55:42 +0200

    subclass cheater Deck for loop card class deck class update

--- src/Deck.java
+++ src/Deck.java
@@ -1,15 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 public class Deck {
     private List<Card> deck;
 
     public Deck(){
         deck = new arrayList<>();
         for (int i = 1; i <= 10; i++){
-            deck.add
+            deck.add(new Card(i));
         }
+        shuffleDeck();
     }
-public Deck(){
-    for (int i = 0; i < deck.length; i++){
-    deck[i] = new Card((i+1));
+public Card drawCard() {
+    return deck.remove(deck.size() - 1);
+}
+private void shuffleDeck(){
+        Collection.shuffle(deck);
     }
 }
-}
+
