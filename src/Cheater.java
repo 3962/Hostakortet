166b131840ba0f2e68502a2ba9cf3c70b63da359
Leonard Bergman <bergman.leonard@elev.ga.ntig.se>
2023-05-29 20:02:07 +0200

    subclass cheater Deck for loop card class deck class update player class cheater

--- src/Cheater.java
+++ src/Cheater.java
@@ -1,2 +1,12 @@
 public class Cheater extends Player {
+    public Cheater(Deck deck){
+        super(deck);
+    }
+
+    @Override
+    public Card drawCard(){
+        Card card1 = deck.drawCard();
+        Card card2 = deck.drawCard();
+
+    }
 }
